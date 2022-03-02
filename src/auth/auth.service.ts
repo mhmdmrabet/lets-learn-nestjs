65d6516c2b20866262df62687a0031a21e7c6fde
Mohamed <mhmdmrabet@yahoo.fr>
2022-03-02 17:42:07 +0100

    Signin logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,6 +6,8 @@ import { AuthDto } from './dto';
 
 @Injectable({})
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
   async signup(dto: AuthDto) {
     // * generate the pwd hash
     const hash = await argon.hash(dto.password);
@@ -33,8 +35,24 @@ export class AuthService {
     }
   }
 
-  constructor(private prisma: PrismaService) {}
-  signin() {
-    return { message: 'Selem, Imran ! You have sign in' };
+  async signin(dto: AuthDto) {
+    // * Find the user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    //   If user does not exist throw exception
+    if (!user) throw new ForbiddenException('Credentials incorrect');
+
+    // * Compare pwd
+    const pwdMatches = await argon.verify(user.hash, dto.password);
+    //   If pwd incorrect throw exception
+    if (!pwdMatches) throw new ForbiddenException('Credentials incorrect');
+
+    delete user.hash;
+
+    // * Send back the user
+    return user;
   }
 }
